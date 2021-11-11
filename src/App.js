84ff683a0c7b47151441f57d209193ee4c1ff091
asai-rupali <asairupali@gmail.com>
2021-11-11 19:46:10 +0530

    assignment one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {title: 'Car Insurance', amount:302.67, date: new Date(2021, 4, 28)},
+    {title: 'Bike Insurance', amount:111.08, date: new Date(2021, 3, 12)},
+    {title: 'Health Insurance', amount:30.61, date: new Date(2021, 4, 7)},
+    {title: 'Home Insurance', amount:3024.64, date: new Date(2021, 5, 20)},
+    {title: 'Travel Insurance', amount:88.91, date: new Date(2021, 1, 18)},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h2>Lets get started</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
